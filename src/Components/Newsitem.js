61cb9da3cbaecff278ee02a6166fe36e411a9a3d
Mrinal Chaubey <mrinalchaubey7@gmail.com>
2025-06-20 01:08:20 +0530

    Modified Newsitem.js content

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -5,20 +5,22 @@ export class Newsitem extends Component {
 
   render() {
     /* from this.props gather title and description */
-    let { title, description, imageurl,newsurl} = this.props;
+    let { title, description, imageurl, newsurl } = this.props;
     return (
       <div>
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageurl} className="card-img-top" alt="..." />
+          <img src={imageurl ?  imageurl :
+    "https://www.tagesspiegel.de/images/13812610/alternates/BASE_16_9_W1400/1749187888000/polizeieinsatz-nach-einbruch-in-einkaufszentrum.jpeg" } className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <a href={newsurl} target="_blank" className ="btn btn-sm btn-primary">  
+            <a rel="noreferrer" href={newsurl} target="_blank" className="btn btn-sm btn-dark">
               Read More...
             </a>
             {/* // here newsurl is javascript object */}
           </div>
         </div>
+      
       </div>
     );
   }
